d8152a9177d42316f3cb52059c73b003baf9bc11
Waleed Hassan <waleedhassankirmani@gmail.com>
2026-03-17 23:52:46 +0500

    basic fetching added to submissions. base set up for future queries

--- backend/controllers/SubmissionsController.js
+++ backend/controllers/SubmissionsController.js
@@ -1,6 +1,26 @@
+import pool from '../config/db.js'
 import express from "express";
 
-export const getAllSubmissions = (req, res) => {
-  //logic to fetch data from db
+async function fetchSubmissions(userId) {
+  try{
+    const result = await pool.query`SELECT * FROM Analysis_Submissions`;
+    return result.recordset;
+  }
+  catch(err){
+    console.log("Failed to fetch Submissions: ", err);
+    return 0;
+  }
+}
+
+export const getAllSubmissions = async (req, res) => {
+  try{
+  const userId = 0;  
+  const data = await fetchSubmissions(userId);
+  console.log(data);
   res.json([{ id: 1, name: "Example submission" }]);
+  }
+  catch (err) {
+    console.log("Failed to get Submissions: ", err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
